60dc8e6cc1a4d259b488a590453d4980f05cdbee
EvilPyro <EvilPyro@protonmail.com>
2021-10-28 17:00:24 +0200

    TextRender gone

--- Sisao/Text.cpp
+++ Sisao/Text.cpp
@@ -16,201 +16,207 @@ bool Text::bLibInit = false;
 FT_Library Text::library;
 
 
-Text::Text()
-{
-	quad = NULL;
-}
+Text::Text() {}
 
-Text::~Text()
-{
-	destroy();
-	if (quad != NULL)
-	{
-		quad->free();
-		delete quad;
-	}
+Text::~Text() {
+    destroy();
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &vao);
 }
 
 
-bool Text::init(const char* filename)
-{
-	FT_Error error;
+bool Text::init(const char* filename) {
+    FT_Error error;
 
-	if (!bLibInit)
-	{
-		error = FT_Init_FreeType(&Text::library);
-		if (error)
-			return false;
-		bLibInit = true;
-	}
-	error = FT_New_Face(Text::library, filename, 0, &face);
-	if (error)
-		return false;
-	FT_Set_Pixel_Sizes(face, ATLAS_FONT_SIZE, ATLAS_FONT_SIZE);
+    if (!bLibInit) {
+        error = FT_Init_FreeType(&Text::library);
+        if (error)
+            return false;
+        bLibInit = true;
+    }
+    error = FT_New_Face(Text::library, filename, 0, &face);
+    if (error)
+        return false;
+    FT_Set_Pixel_Sizes(face, ATLAS_FONT_SIZE, ATLAS_FONT_SIZE);
 
-	if (!extractCharSizes(&maxCharWidth, &maxCharHeight))
-		return false;
-	fontSize = maxCharHeight;
-	textureSize = 512;
-	if (floor(float(textureSize) / maxCharWidth) * floor(float(textureSize) / maxCharHeight) < (128 - 32))
-		textureSize = 1024;
-	if (floor(float(textureSize) / maxCharWidth) * floor(float(textureSize) / maxCharHeight) < (128 - 32))
-		return false;
-	createTextureAtlas();
-	initShaders();
+    if (!extractCharSizes(&maxCharWidth, &maxCharHeight))
+        return false;
+    fontSize = maxCharHeight;
+    textureSize = 512;
+    if (floor(float(textureSize) / maxCharWidth) * floor(float(textureSize) / maxCharHeight) < (128 - 32))
+        textureSize = 1024;
+    if (floor(float(textureSize) / maxCharWidth) * floor(float(textureSize) / maxCharHeight) < (128 - 32))
+        return false;
+    createTextureAtlas();
+    initShaders();
 
-	glm::vec2 geom[2] = { glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f) };
-	glm::vec2 texCoords[2] = { glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f) };
+    glm::vec2 geom[2] = { glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f) };
+    glm::vec2 texCoords[2] = { glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f) };
 
-	quad = TextRender::createTexturedQuad(geom, texCoords, program);
+    float vertices[24] = { geom[0].x, geom[0].y, texCoords[0].x, texCoords[0].y,
+                                                geom[1].x, geom[0].y, texCoords[1].x, texCoords[0].y,
+                                                geom[1].x, geom[1].y, texCoords[1].x, texCoords[1].y,
+                                                geom[0].x, geom[0].y, texCoords[0].x, texCoords[0].y,
+                                                geom[1].x, geom[1].y, texCoords[1].x, texCoords[1].y,
+                                                geom[0].x, geom[1].y, texCoords[0].x, texCoords[1].y };
 
-	return true;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(float), vertices, GL_STATIC_DRAW);
+    posLocation = program.bindVertexAttribute("position", 2, 4 * sizeof(float), 0);
+    texCoordLocation = program.bindVertexAttribute("texCoord", 2, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+    return true;
 }
 
-void Text::destroy()
-{
-	FT_Done_Face(face);
+void Text::destroy() {
+    FT_Done_Face(face);
 }
 
-ShaderProgram& Text::getProgram()
-{
-	return program;
+ShaderProgram& Text::getProgram() {
+    return program;
 }
 
-int Text::getSize() const
-{
-	return fontSize;
+int Text::getSize() const {
+    return fontSize;
 }
 
-void Text::render(char c, const glm::vec2& pixel, int size, const glm::vec4& color)
-{
-	int vp[4];
-	glm::mat4 projection, modelview;
-	glm::vec2 minTexCoord, maxTexCoord;
+void Text::render(char c, const glm::vec2& pixel, int size, const glm::vec4& color) {
+    int vp[4];
+    glm::mat4 projection, modelview;
+    glm::vec2 minTexCoord, maxTexCoord;
 
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
-	program.use();
-	glGetIntegerv(GL_VIEWPORT, vp);
-	projection = glm::ortho(0.f, float(vp[2] - 1), float(vp[3] - 1), 0.f);
-	program.setUniformMatrix4f("projection", projection);
-	program.setUniform4f("color", color.r, color.g, color.b, color.a);
-	modelview = glm::mat4(1.0f);
-	modelview = glm::translate(modelview, glm::vec3(pixel.x, pixel.y - size, 0.f));
-	modelview = glm::scale(modelview, (float(size) / fontSize) * glm::vec3(chars[c - 32].sx, chars[c - 32].sy, 0.f));
-	program.setUniformMatrix4f("modelview", modelview);
-	minTexCoord = glm::vec2(float(chars[c - 32].tx) / textureSize, float(chars[c - 32].ty) / textureSize);
-	maxTexCoord = glm::vec2(float(chars[c - 32].tx + chars[c - 32].sx) / textureSize, float(chars[c - 32].ty + chars[c - 32].sy) / textureSize);
-	program.setUniform2f("minTexCoord", minTexCoord.s, minTexCoord.t);
-	program.setUniform2f("maxTexCoord", maxTexCoord.s, maxTexCoord.t);
-	quad->render(textureAtlas);
-	glDisable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    program.use();
+    glGetIntegerv(GL_VIEWPORT, vp);
+    projection = glm::ortho(0.f, float(vp[2] - 1), float(vp[3] - 1), 0.f);
+    program.setUniformMatrix4f("projection", projection);
+    program.setUniform4f("color", color.r, color.g, color.b, color.a);
+    modelview = glm::mat4(1.0f);
+    modelview = glm::translate(modelview, glm::vec3(pixel.x, pixel.y - size, 0.f));
+    modelview = glm::scale(modelview, (float(size) / fontSize) * glm::vec3(chars[c - 32].sx, chars[c - 32].sy, 0.f));
+    program.setUniformMatrix4f("modelview", modelview);
+    minTexCoord = glm::vec2(float(chars[c - 32].tx) / textureSize, float(chars[c - 32].ty) / textureSize);
+    maxTexCoord = glm::vec2(float(chars[c - 32].tx + chars[c - 32].sx) / textureSize, float(chars[c - 32].ty + chars[c - 32].sy) / textureSize);
+    program.setUniform2f("minTexCoord", minTexCoord.s, minTexCoord.t);
+    program.setUniform2f("maxTexCoord", maxTexCoord.s, maxTexCoord.t);
+
+    glEnable(GL_TEXTURE_2D);
+    textureAtlas.use();
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(posLocation);
+    glEnableVertexAttribArray(texCoordLocation);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDisable(GL_TEXTURE_2D);
+
+
+    glDisable(GL_BLEND);
 }
 
-void Text::render(const string& str, const glm::vec2& pixel, int size, const glm::vec4& color)
-{
-	int vp[4];
-	glm::mat4 projection, modelview;
-	glm::vec2 minTexCoord, maxTexCoord, pos = pixel;
+void Text::render(const string& str, const glm::vec2& pixel, int size, const glm::vec4& color) {
+    int vp[4];
+    glm::mat4 projection, modelview;
+    glm::vec2 minTexCoord, maxTexCoord, pos = pixel;
 
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
-	program.use();
-	glGetIntegerv(GL_VIEWPORT, vp);
-	projection = glm::ortho(0.f, float(vp[2] - 1), float(vp[3] - 1), 0.f);
-	program.setUniformMatrix4f("projection", projection);
-	program.setUniform4f("color", color.r, color.g, color.b, color.a);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    program.use();
+    glGetIntegerv(GL_VIEWPORT, vp);
+    projection = glm::ortho(0.f, float(vp[2] - 1), float(vp[3] - 1), 0.f);
+    program.setUniformMatrix4f("projection", projection);
+    program.setUniform4f("color", color.r, color.g, color.b, color.a);
 
-	for (unsigned int i = 0; i < str.length(); i++)
-	{
-		modelview = glm::mat4(1.0f);
-		modelview = glm::translate(modelview, glm::vec3(pos.x + (float(size) / fontSize) * chars[str[i] - 32].bl, pos.y - (float(size) / fontSize) * chars[str[i] - 32].bt, 0.f));
-		modelview = glm::scale(modelview, (float(size) / fontSize) * glm::vec3(chars[str[i] - 32].sx, chars[str[i] - 32].sy, 0.f));
-		program.setUniformMatrix4f("modelview", modelview);
-		minTexCoord = glm::vec2(float(chars[str[i] - 32].tx) / textureSize, float(chars[str[i] - 32].ty) / textureSize);
-		maxTexCoord = glm::vec2(float(chars[str[i] - 32].tx + chars[str[i] - 32].sx) / textureSize, float(chars[str[i] - 32].ty + chars[str[i] - 32].sy) / textureSize);
-		program.setUniform2f("minTexCoord", minTexCoord.s, minTexCoord.t);
-		program.setUniform2f("maxTexCoord", maxTexCoord.s, maxTexCoord.t);
-		quad->render(textureAtlas);
-		pos.x += (float(size) / fontSize) * chars[str[i] - 32].ax;
-	}
+    for (unsigned int i = 0; i < str.length(); i++) {
+        modelview = glm::mat4(1.0f);
+        modelview = glm::translate(modelview, glm::vec3(pos.x + (float(size) / fontSize) * chars[str[i] - 32].bl, pos.y - (float(size) / fontSize) * chars[str[i] - 32].bt, 0.f));
+        modelview = glm::scale(modelview, (float(size) / fontSize) * glm::vec3(chars[str[i] - 32].sx, chars[str[i] - 32].sy, 0.f));
+        program.setUniformMatrix4f("modelview", modelview);
+        minTexCoord = glm::vec2(float(chars[str[i] - 32].tx) / textureSize, float(chars[str[i] - 32].ty) / textureSize);
+        maxTexCoord = glm::vec2(float(chars[str[i] - 32].tx + chars[str[i] - 32].sx) / textureSize, float(chars[str[i] - 32].ty + chars[str[i] - 32].sy) / textureSize);
+        program.setUniform2f("minTexCoord", minTexCoord.s, minTexCoord.t);
+        program.setUniform2f("maxTexCoord", maxTexCoord.s, maxTexCoord.t);
 
-	glDisable(GL_BLEND);
+        glEnable(GL_TEXTURE_2D);
+        textureAtlas.use();
+        glBindVertexArray(vao);
+        glEnableVertexAttribArray(posLocation);
+        glEnableVertexAttribArray(texCoordLocation);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDisable(GL_TEXTURE_2D);
+
+        pos.x += (float(size) / fontSize) * chars[str[i] - 32].ax;
+    }
+
+    glDisable(GL_BLEND);
 }
 
-void Text::initShaders()
-{
-	Shader vShader, fShader;
+void Text::initShaders() {
+    Shader vShader, fShader;
 
-	vShader.release();
-	fShader.release();
-	vShader.initFromFile(VERTEX_SHADER, "shaders/text.vert");
-	if (!vShader.isCompiled())
-	{
-		cout << "Vertex Shader Error" << endl;
-		cout << "" << vShader.log() << endl << endl;
-	}
-	fShader.initFromFile(FRAGMENT_SHADER, "shaders/text.frag");
-	if (!fShader.isCompiled())
-	{
-		cout << "Fragment Shader Error" << endl;
-		cout << "" << fShader.log() << endl << endl;
-	}
-	program.init();
-	program.addShader(vShader);
-	program.addShader(fShader);
-	program.link();
-	if (!program.isLinked())
-	{
-		cout << "Shader Linking Error" << endl;
-		cout << "" << program.log() << endl << endl;
-	}
-	program.bindFragmentOutput("outColor");
+    vShader.release();
+    fShader.release();
+    vShader.initFromFile(VERTEX_SHADER, "shaders/text.vert");
+    if (!vShader.isCompiled()) {
+        cout << "Vertex Shader Error" << endl;
+        cout << "" << vShader.log() << endl << endl;
+    }
+    fShader.initFromFile(FRAGMENT_SHADER, "shaders/text.frag");
+    if (!fShader.isCompiled()) {
+        cout << "Fragment Shader Error" << endl;
+        cout << "" << fShader.log() << endl << endl;
+    }
+    program.init();
+    program.addShader(vShader);
+    program.addShader(fShader);
+    program.link();
+    if (!program.isLinked()) {
+        cout << "Shader Linking Error" << endl;
+        cout << "" << program.log() << endl << endl;
+    }
+    program.bindFragmentOutput("outColor");
 }
 
-bool Text::extractCharSizes(int* maxCharWidth, int* maxCharHeight)
-{
-	unsigned char c;
+bool Text::extractCharSizes(int* maxCharWidth, int* maxCharHeight) {
+    unsigned char c;
 
-	*maxCharWidth = 0;
-	*maxCharHeight = 0;
-	for (c = 32; c < 128; c++)
-	{
-		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
-			return false;
-		*maxCharWidth = glm::max(*maxCharWidth, (int)face->glyph->bitmap.width);
-		*maxCharHeight = glm::max(*maxCharHeight, (int)face->glyph->bitmap.rows);
-	}
+    *maxCharWidth = 0;
+    *maxCharHeight = 0;
+    for (c = 32; c < 128; c++) {
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+            return false;
+        *maxCharWidth = glm::max(*maxCharWidth, (int)face->glyph->bitmap.width);
+        *maxCharHeight = glm::max(*maxCharHeight, (int)face->glyph->bitmap.rows);
+    }
 
-	return true;
+    return true;
 }
 
-void Text::createTextureAtlas()
-{
-	unsigned char c;
-	int x = 0, y = 0;
+void Text::createTextureAtlas() {
+    unsigned char c;
+    int x = 0, y = 0;
 
-	textureAtlas.createEmptyTexture(textureSize, textureSize);
-	for (c = 32; c < 128; c++)
-	{
-		FT_Load_Char(face, c, FT_LOAD_RENDER);
-		chars[c - 32].tx = x;
-		chars[c - 32].ty = y;
-		chars[c - 32].sx = face->glyph->bitmap.width;
-		chars[c - 32].sy = face->glyph->bitmap.rows;
-		chars[c - 32].ax = face->glyph->advance.x >> 6;
-		chars[c - 32].ay = face->glyph->advance.y >> 6;
-		chars[c - 32].bl = face->glyph->bitmap_left;
-		chars[c - 32].bt = face->glyph->bitmap_top;
-		textureAtlas.loadSubtextureFromGlyphBuffer(face->glyph->bitmap.buffer, x, y, face->glyph->bitmap.width, face->glyph->bitmap.rows);
-		x += maxCharWidth;
-		if ((x + maxCharWidth) >= textureSize)
-		{
-			x = 0;
-			y += maxCharHeight;
-		}
-	}
-	textureAtlas.generateMipmap();
-	textureAtlas.setWrapS(GL_CLAMP_TO_EDGE);
-	textureAtlas.setWrapT(GL_CLAMP_TO_EDGE);
+    textureAtlas.createEmptyTexture(textureSize, textureSize);
+    for (c = 32; c < 128; c++) {
+        FT_Load_Char(face, c, FT_LOAD_RENDER);
+        chars[c - 32].tx = x;
+        chars[c - 32].ty = y;
+        chars[c - 32].sx = face->glyph->bitmap.width;
+        chars[c - 32].sy = face->glyph->bitmap.rows;
+        chars[c - 32].ax = face->glyph->advance.x >> 6;
+        chars[c - 32].ay = face->glyph->advance.y >> 6;
+        chars[c - 32].bl = face->glyph->bitmap_left;
+        chars[c - 32].bt = face->glyph->bitmap_top;
+        textureAtlas.loadSubtextureFromGlyphBuffer(face->glyph->bitmap.buffer, x, y, face->glyph->bitmap.width, face->glyph->bitmap.rows);
+        x += maxCharWidth;
+        if ((x + maxCharWidth) >= textureSize) {
+            x = 0;
+            y += maxCharHeight;
+        }
+    }
+    textureAtlas.generateMipmap();
+    textureAtlas.setWrapS(GL_CLAMP_TO_EDGE);
+    textureAtlas.setWrapT(GL_CLAMP_TO_EDGE);
 }
--- Sisao/Text.h
+++ Sisao/Text.h
@@ -4,18 +4,16 @@
 #include FT_FREETYPE_H
 #include "Texture.h"
 #include "ShaderProgram.h"
-#include "TextRender.h"
 
 
 #define NUM_CHARACTERS (128 - 32)
 
 
-struct CharMetrics
-{
-	int tx, ty; 		// Pos x, y inside texture in pixels
-	int sx, sy; 		// Size of char in pixels
-	int ax, ay; 		// Advance for each character in a string sequence
-	int bl, bt; 		// Bitmap left and top adjusting
+struct CharMetrics {
+    int tx, ty; 		// Pos x, y inside texture in pixels
+    int sx, sy; 		// Size of char in pixels
+    int ax, ay; 		// Advance for each character in a string sequence
+    int bl, bt; 		// Bitmap left and top adjusting
 };
 
 
@@ -24,35 +22,37 @@ struct CharMetrics
 // to render text strings
 
 
-class Text
-{
+class Text {
 
 public:
-	Text();
-	~Text();
+    Text();
+    ~Text();
 
-	bool init(const char* filename);
-	void destroy();
+    bool init(const char* filename);
+    void destroy();
 
-	ShaderProgram& getProgram();
-	int getSize() const;
-	void render(char c, const glm::vec2& pixel, int size, const glm::vec4& color);
-	void render(const string& str, const glm::vec2& pixel, int size, const glm::vec4& color);
-
-private:
-	void initShaders();
-	bool extractCharSizes(int* maxCharWidth, int* maxCharHeight);
-	void createTextureAtlas();
+    ShaderProgram& getProgram();
+    int getSize() const;
+    void render(char c, const glm::vec2& pixel, int size, const glm::vec4& color);
+    void render(const string& str, const glm::vec2& pixel, int size, const glm::vec4& color);
 
 private:
-	int fontSize, textureSize, maxCharWidth, maxCharHeight;
-	FT_Face face;
-	CharMetrics chars[NUM_CHARACTERS];
-	Texture textureAtlas;
-	ShaderProgram program;
-	TextRender* quad;
+    void initShaders();
+    bool extractCharSizes(int* maxCharWidth, int* maxCharHeight);
+    void createTextureAtlas();
 
-	static bool bLibInit;
-	static FT_Library library;
+private:
+    GLuint vao;
+    GLuint vbo;
+    GLint posLocation, texCoordLocation;
+
+    int fontSize, textureSize, maxCharWidth, maxCharHeight;
+    FT_Face face;
+    CharMetrics chars[NUM_CHARACTERS];
+    Texture textureAtlas;
+    ShaderProgram program;
+
+    static bool bLibInit;
+    static FT_Library library;
 
 };
