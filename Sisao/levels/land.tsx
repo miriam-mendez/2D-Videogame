5c59925909edb9aabc4e5fba5d8c3ab56545f347
Miriam Mendez <miriam.mendez.s@gmail.com>
2021-10-28 21:32:56 +0200

    menu

---
+++ Sisao/levels/land.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="land" tilewidth="16" tileheight="16" tilecount="28" columns="4">
+ <image source="../images/wasteland.PNG" trans="000000" width="64" height="112"/>
+</tileset>
